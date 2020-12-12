2a46ea1ecf6af54951b72a75279f0268d9b5006a
Alfian Isnan <fianshared@gmail.com>
2020-12-12 20:13:25 +0700

    Update Files

--- automatic_wash_basin.ino
+++ automatic_wash_basin.ino
@@ -1,11 +1,14 @@
 #include "pin.h";
 #include "alfian.h";
+bool state = false;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Initialize Weight");
   WEIGHT.init();
 }
 
 void loop() {
-  Serial.print(WEIGHT.getValue());
+  Serial.println(WEIGHT.getValue());
+  delay(1000);
 }
